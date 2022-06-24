be15cf05a266a70721d68c1cf792703201ae412a
IceQ1337 <IceQ1337@gmx.de>
2022-06-24 22:35:15 +0200

    Update php-cs-fixer config
    This PSR2 rule is driving me insane in almost every project.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -117,5 +117,7 @@ return (new PhpCsFixer\Config())
         'method_chaining_indentation' => true,
         'no_extra_blank_lines' => ['tokens' => ['case', 'continue', 'curly_brace_block', 'default', 'extra', 'parenthesis_brace_block', 'square_brace_block', 'switch', 'throw', 'use']],
         'no_spaces_around_offset' => true,
+
+        'no_whitespace_in_blank_line' => true,
     ])
     ->setFinder($finder);
